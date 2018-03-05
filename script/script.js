6555ecdb6bb84f9e5b202c0eaf5e145936f610fe
ammorris <37010476+ammorris@users.noreply.github.com>
2018-03-04 19:33:04 -0800

    +Clock & +Nav titles
    Added clock to nav, and gave titles to buttons.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 var decimalPhase = SunCalc.getMoonIllumination(new Date()).phase;
 var phase = decimalToPhase(decimalPhase);
 var infoBox = document.getElementsByClassName("info")[0];
+const clock = document.getElementById("clock");
 
 // Display messages
 var message1 = document.createElement('p');
@@ -48,74 +49,26 @@ function getPhaseImg(decimalPhase) {
     return fileName;
 }
 
-// // A function to return the approximate current lunar phase
-// function getPhase(decimalPhase) {
-//     var range1 = decimalPhase >= 0.0 && decimalPhase < 0.1;
-//     var range2 = decimalPhase >= 0.1 && decimalPhase < 0.2;
-//     var range3 = decimalPhase >= 0.2 && decimalPhase < 0.3;
-//     var range4 = decimalPhase >= 0.3 && decimalPhase < 0.4;
-//     var range5 = decimalPhase >= 0.4 && decimalPhase < 0.55;
-//     var range6 = decimalPhase >= 0.55 && decimalPhase < 0.7;
-//     var range7 = decimalPhase >= 0.7 && decimalPhase < 0.8;
-//     var range8 = decimalPhase >= 0.8 && decimalPhase <= 1.0;
+function blink() {
+    var colon = document.getElementById("colon");
+    colon.style.visibility = colon.style.visibility == "hidden" ? "visible" : "hidden";
+}
 
-//     if (range1) { return "New Moon"; } 
-//     else if (range2) { return "Waxing Crescent"; } 
-//     else if (range3) { return "First Quarter"; } 
-//     else if (range4) { return "Waxing Gibbous"; } 
-//     else if (range5) { return "Full Moon"; }
-//     else if (range6) { return "Waning Gibbous"; } 
-//     else if (range7) { return "Last Quarter"; } 
-//     else if (range8) { return "Waning Crescent"; }
-//     return error;
-// }
+document.body.onload = function() {
+    updateClock();
+    setInterval("updateClock()", 1000);
+    setInterval("blink()", 500);
+}
 
-// var message1 = document.createElement('p');
-// message1.innerHTML = "&#9907; Current moon phase is " + decimalPhase.toFixed(2) + " &#9910;";
-// document.getElementsByClassName("info")[0].appendChild(message1);
-// var message2 = document.createElement('p');
-// message2.innerHTML = phase;
-// document.getElementsByClassName("info")[0].appendChild(message2);
-
-// var image = document.createElement('img');
-// var displayBox = document.getElementsByClassName("info")[0];
-
-// if (phase.toLowerCase() == "new moon") {
-//     image.src = "img/newMoon.svg";
-//     displayBox.appendChild(image);
-// }
-
-// else if (phase.toLowerCase() == "waxing crescent") {
-//     image.src = "img/waxingCrescent.svg";
-//     displayBox.appendChild(image);
-// }
-
-// else if (phase.toLowerCase() == "first quarter") {
-//     image.src = "img/firstQuarter.svg";
-//     displayBox.appendChild(image);
-// }
-
-// else if (phase.toLowerCase() == "waxing gibbous") {
-//     image.src = "img/waxingGibbous.svg";
-//     displayBox.appendChild(image);
-// }
-
-// else if (phase.toLowerCase() == "full moon") {
-//     image.src = "img/fullMoon.svg";
-//     displayBox.appendChild(image);
-// }
-
-// else if (phase.toLowerCase() == "waning gibbous") {
-//     image.src = "img/waningGibbous.svg";
-//     displayBox.appendChild(image);
-// }
-
-// else if (phase.toLowerCase() == "las quarter") {
-//     image.src = "img/lastQuarter.svg";
-//     displayBox.appendChild(image);
-// }
-
-// else if (phase.toLowerCase() == "waning crescent") {
-//     image.src = "img/waningCrescent.svg";
-//     displayBox.appendChild(image);
-// }
+function updateClock() {
+    var today = new Date();
+    var hours = today.getHours() % 12;
+    if (hours == 0) { hours = 12; }
+    var minutes = today.getMinutes();
+    var timeOfDay = (today.getHours()) < 12 ? "午前" : "午後";
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    var time = hours + "<span id='colon'>:</span>" + minutes + timeOfDay;
+    document.getElementById('clock').innerHTML = time;
+}
